3d7740e7cbdfed86fd519611b4fa8c53c2ee12a7
AnnySad <anna.sadovskaya555@gmail.com>
2021-05-05 20:39:45 +0300

    71-setState, local state

---
+++ src/Components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import p from '../ProfileStatus.module.css';
+import Tenor from "../../common/tenor/tenor";
+
+export type ProfileStatusPropsType = {
+    status: any
+}
+
+class ProfileStatus extends React.Component <ProfileStatusPropsType> {
+
+    state = {
+        editMode: false,
+        title: 'yo'
+
+    }
+
+    activateEditMode() {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deactivateEditMode() {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+        return <div>
+            {!this.state.editMode &&
+            <div>
+                <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+            </div>
+            }
+            {this.state.editMode &&
+            <div>
+                <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+            </div>
+            }
+        </div>
+    }
+}
+
+export default ProfileStatus;
