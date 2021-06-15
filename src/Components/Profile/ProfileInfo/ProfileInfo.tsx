ecc6bc0fefa38a82246283f04453118f97b2f86c
AnnySad <anna.sadovskaya555@gmail.com>
2021-06-15 20:43:33 +0300

    84 - React JS - hook, useState, хуки

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Tenor from "../../common/tenor/tenor";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 
 type ProfileInfoPropsType = {
@@ -21,7 +22,7 @@ const ProfileInfo = (props:ProfileInfoPropsType) => {
 
         <div className={s.descriptionBlock}>
             <img src={props.profile.photos.large}/>
-            <ProfileStatus  status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks  status={props.status} updateStatus={props.updateStatus}/>
         </div>
     </div>
 }
